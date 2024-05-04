aabaa3e8ece7ef00783aaaff588f7b1680a9c916
Pranjal <pranjal17pr@gmail.com>
2024-05-04 12:59:21 +0530

    Min function

--- src/main/java/demo/MyCalculator.java
+++ src/main/java/demo/MyCalculator.java
@@ -4,8 +4,12 @@ public class MyCalculator {
 	public int sum(int a ,int b) {
 		return a+b;
 	}
+	public int min(int a ,int b) {
+		return a-b;
+	}
 	public static void main (String[] args){
 		MyCalculator calc = new MyCalculator();
 		System.out.println("Sum is"+calc.sum(20, 10));
+		System.out.println("Min is"+calc.min(20, 10));
 	}
 }
